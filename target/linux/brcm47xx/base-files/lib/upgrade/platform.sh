828a107547f72ee9f1dc379343d67e3d1ae7e267
Cody P Schafer <devel@codyps.com>
2013-11-09 22:34:27 -0800

    brcm47xx/upgrade: add workarounds for Belkin .trx headers
    Signed-off-by: Cody P Schafer <devel@codyps.com>

--- target/linux/brcm47xx/base-files/lib/upgrade/platform.sh
+++ target/linux/brcm47xx/base-files/lib/upgrade/platform.sh
@@ -1,14 +1,39 @@
 PART_NAME=linux
 
+get_fw_mtd () {
+	grep "\"$PART_NAME\"" < /proc/mtd | cut -d':' -f1
+}
+
+get_current_magic_long () {
+	dd bs=4 count=1 2>/dev/null < /dev/"$(get_fw_mtd)" | hexdump -v -n 4 -e '1/1 "%02x"'
+}
+
 platform_check_image() {
 	[ "$ARGC" -gt 1 ] && return 1
 
-	case "$(get_magic_word "$1")" in
-		# .trx files
-		4844) return 0;;
-		*)
-			echo "Invalid image type. Please use only .trx files"
+	magic="$(get_magic_long "$1")"
+	# For Belkin support, check on magic in current image.
+	cmagic="$(get_current_magic_long)"
+
+	case "$cmagic" in
+	78563412) # Belkin F7Dxxxx QA Firmware
+		if [ $magic = 78563412 ] ; then
+			return 0
+		else
+			echo "Invalid image for this router."
+			echo "Please use a openwrt-f7dxxxx-*.bin file"
 			return 1
+		fi
+		;;
+	*)
+		case "$magic" in
+			# .trx files
+			48445230) return 0;;
+			*)
+				echo "Invalid image type. Please use only .trx files"
+				return 1
+			;;
+		esac
 		;;
 	esac
 }
